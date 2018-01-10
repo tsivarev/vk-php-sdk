a7f4eff827869f826fecbc8f60f4e7095e529e59
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2018-01-10 16:19:28 +0300

    code refactoring

--- src/VK/VKClient.php
+++ src/VK/VKClient.php
@@ -14,6 +14,7 @@ class VKClient {
     const CONNECTION_TIMEOUT = 10;
     const VERSION_PARAM = 'v';
     const ACCESS_TOKEN_PARAM = 'access_token';
+    const CONTENT_TYPE_HEADER = 'Content-Type:multipart/form-data';
 
     protected $api_version = '5.69';
 
@@ -45,41 +46,42 @@ class VKClient {
     /**
      *
      * @param string $upload_url
-     * @param string $photoPath
+     * @param string $type
+     * @param string $path
      *
      * @return VKResponse
      *
      * @throws VKClientException
      */
-    public function postPhoto($upload_url, $photoPath) {
-        $files = array();
-        $files['photo'] = (class_exists('CURLFile', false)) ?
-            new \CURLFile($photoPath) : '@' . $photoPath;
+    public function upload($upload_url, $type, $path) {
+        $payload = array();
+        $payload[$type] = (class_exists('CURLFile', false)) ?
+            new \CURLFile($path) : '@' . $path;
 
         return $this->sendRequest($upload_url, array(
             CURLOPT_POST => 1,
             CURLOPT_HTTPHEADER => array(
-                "Content-Type:multipart/form-data"
+                static::CONTENT_TYPE_HEADER,
             ),
             CURLOPT_HEADER => true,
             CURLOPT_CONNECTTIMEOUT => static::CONNECTION_TIMEOUT,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POSTFIELDS => $files,
+            CURLOPT_POSTFIELDS => $payload,
         ));
     }
 
     /**
      *
      * @param string $url
-     * @param array $opt_array
+     * @param array $options
      *
      * @return VKResponse
      *
      * @throws VKClientException
      */
-    protected function sendRequest($url, $opt_array) {
+    protected function sendRequest($url, $options) {
         $curl = curl_init($url);
-        curl_setopt_array($curl, $opt_array);
+        curl_setopt_array($curl, $options);
         $raw_response = curl_exec($curl);
 
         $response = new VKResponse($raw_response);
