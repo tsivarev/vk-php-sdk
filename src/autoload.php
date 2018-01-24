c4f0709f760cfbc2f52aaf7695b660724ce1bfc0
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2018-01-24 17:16:14 +0300

    Delete autoload.php

--- src/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function ($class) {
-    $filename = trim(str_replace('\\','/', $class), '/') . '.php';
-    if (file_exists(__DIR__ . '/' .$filename)) {
-        require_once $filename;
-    }
-});
